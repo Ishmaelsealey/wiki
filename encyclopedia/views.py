0cf5e51eb8153bdb314313a0212b8fc57897e815
Ishmaelsealey <64021138+Ishmaelsealey@users.noreply.github.com>
2021-01-13 16:19:29 -0400

    working on getting the css to render like proper html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,14 +4,13 @@ from . import util
 
 from markdown2 import Markdown
 
-markdowner = Markdown()
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, title):
+    markdowner = Markdown()
     return render(request, "encyclopedia/entries.html", {
         "title": markdowner.convert(util.get_entry(f"{title}"))
     })
