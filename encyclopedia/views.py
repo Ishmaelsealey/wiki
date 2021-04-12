e39feb5b645b74cbdb580cf922f683174eecd5d6
Ishmaelsealey <64021138+Ishmaelsealey@users.noreply.github.com>
2021-04-11 21:39:09 -0400

    i didn't commit these and don't know what I did...

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
 from . import util
-
 from markdown2 import Markdown
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -15,5 +16,20 @@ def entry(request, title):
         "title": markdowner.convert(util.get_entry(f"{title}"))
     })
 
+class NewEntryForm(forms.Form):
+    entry = forms.CharField(label="New Entry")
+
 def new(request):
-    return render(request, "encyclopedia/new.html")
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data["entry"]
+            entry.append(entry)
+            return HttpResponseRedirect(reverse("encyclopedia:index"))
+        else:
+            return render(request, "encyclopedia/new.html", {
+                "form": form
+            })
+    return render(request, "encyclopedia/new.html", {
+        "form": NewEntryForm()
+    })
