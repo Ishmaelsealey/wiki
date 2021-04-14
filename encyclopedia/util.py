7cd5fc78715c12ada526f4c68bbd8b7a590a76af
Ishmaelsealey <64021138+Ishmaelsealey@users.noreply.github.com>
2021-04-13 22:38:14 -0400

    realised that moving the entries folder broke my code... moved it back

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -8,7 +8,7 @@ def list_entries():
     """
     Returns a list of all names of encyclopedia entries.
     """
-    _, filenames = default_storage.listdir("entries")
+    filenames = default_storage.listdir("entries")
     return list(sorted(re.sub(r"\.md$", "", filename)
                 for filename in filenames if filename.endswith(".md")))
 
@@ -18,7 +18,7 @@ def save_entry(title, content):
     Saves an encyclopedia entry, given its title and Markdown
     content. If an existing entry with the same title already exists,
     it is replaced.
-    """ 
+    """
     filename = f"entries/{title}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
